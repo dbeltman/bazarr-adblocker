8fee11f1fa0faddf22b2bd9b050a9f786632c7ec
Daan <daan.w.beltman@gmail.com>
2021-12-11 17:15:39 +0100

    writefile

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import srt, re
-filepath='test.srt'
+filepath='test/srt/test.nl.srt'
 subtitle_generator = srt.parse(open(filepath))
 subtitles= list(subtitle_generator)
 urlregex='https?://\S+|WWW\.\S+'
+counter=0
 for sub in subtitles:
     if re.search(urlregex, sub.content):
+        counter+=1
         print("FOUND ILLEGAL STRING: " + sub.content + " INDEX: " + str(sub.index))
-        subtitles.pop(sub.index-1)
-        print(srt.compose(subtitles))
+        subtitles.pop(sub.index-counter)
+
+print(srt.compose(subtitles))
+finalfile = open("{}.forced.srt".format(filepath.split(".srt")[0]), "w")
+finalfile.write(srt.compose(subtitles))
