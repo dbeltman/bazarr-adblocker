be052bd45de1a2bdf4bc02aa2262ffd003fca8b0
Daan <daan.w.beltman@gmail.com>
2021-12-22 22:22:48 +0100

    debug #4

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-pip3 install -r /scripts/postprocess/requirements.txt
+cd /scripts/postprocess
+if pip3 install -r /scripts/postprocess/requirements.txt
 echo $(whoami)
 python3 /scripts/postprocess/main.py $1 $2 $3 $4
