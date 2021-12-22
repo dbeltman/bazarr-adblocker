168f363b58cf086e31ab4161f573481aaa8ea77c
Daan B <daan.w.beltman@gmail.com>
2021-12-22 22:33:20 +0100

    improve command wrapper

--- main.sh
+++ main.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd /scripts/postprocess
 pip3 install -r /scripts/postprocess/requirements.txt
-echo $(whoami)
-python3 /scripts/postprocess/main.py $1 $2 $3 $4
+python3 /scripts/postprocess/main.py -rw "$1"
