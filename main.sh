bd1e8cb47a6a6f1e08c7cd56892bb88a0ee9fcab
Daan <daan.w.beltman@gmail.com>
2021-12-22 22:36:37 +0100

    usercheck

--- main.sh
+++ main.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [[ $(whoami) != "root" ]] ; then
+    echo "NOT RUNNING AS ROOT"
+    exit 1
+fi
 cd /scripts/postprocess
 pip3 install -r /scripts/postprocess/requirements.txt
 echo $(whoami)
